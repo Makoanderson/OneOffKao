8c0df86ef7ab9cafbe1e6919f4a10de1baa6fc24
makoy <yoshidamaryoshidmarad1234@gmail.com>
2022-08-11 23:50:26 +0900

    フロントページ、アーカイブページ、ブログページのphpファイル作成

--- index.php
+++ index.php
@@ -1,63 +1,162 @@
     <?php get_header();?>   
-            <main class="l-Main p-front c-title">
+            <!--<main class="l-Main p-front c-title">
                 <section class="p-front-info">
-                <?php if(have_posts()):while(have_posts()):the_post();?>
+                
                 <article>
                     <div class="c-title-info">
-                        <!--<img class="c-title-info__img1" src="../img/GerberaPNG/g02.png">
-                        <img class="c-title-info__img2" src="../img/GerberaPNG/g03.png"> -->
+                        <img class="c-title-info__img1" src="../img/GerberaPNG/g02.png">
+                        <img class="c-title-info__img2" src="../img/GerberaPNG/g03.png">
                         <h2><span>新着情報</span></h2>
                     </div>
                     <div class="p-front-info1">
                         <div class="p-front-info1__title">
-                            <img src="<?php echo esc_url( get_template_directory_uri() );?>/img/sakura-series.jpg">
+                            <img src="../img/sakura-series.jpg">
                             <p class="info-title"><a href="#">テキストテキストテキスト</a></p>
                         </div>
-                        <!--<div class="p-front-info1__title">
+                        <div class="p-front-info1__title">
                             <img src="/img/sakura-series.jpg">
                             <p class="info-title"><a href="#">テキストテキストテキスト</a></p>
                         </div>
                         <div class="p-front-info1__title">
                             <img src="/img/sakura-series.jpg" class="info-img1">
                             <p class="info-title info-title-sp"><a href="#">テキストテキストテキスト</a></p>
-                        </div>-->
+                        </div>
                     </div>
                     </article>
-                    <?php endwhile; else:?>
                         <p>記事はありません。</p>
-                    <?php endif;?>
                 </section>
                 <section class="p-front-about">
                     <div class="c-title-about">
-                        <!--<img class="c-title-about__img1" src="../img/GerberaPNG/g02.png">
-                        <img class="c-title-about__img2" src="../img/GerberaPNG/g03.png">-->
+                        <img class="c-title-about__img1" src="../img/GerberaPNG/g02.png">
+                        <img class="c-title-about__img2" src="../img/GerberaPNG/g03.png">
                         <h2><span>About</span></h2>
                     </div>
                     <div class="p-front-about-info">
                         <p>One-Off KAO(ワンオフ・カオ)は、<br>革のお花のアクセサリーショップです。<br>全てハンドメイドで、<br>大きな一枚革からはさみで<br>ひとつずつ切り染めてお花に仕立てています。<br>【美しい色合いと立体感】のある形が特徴です。</p>
-                        <img class="p-front-about__img" src="<?php echo esc_url( get_template_directory_uri() );?>/img/blog2.jpg">
+                        <img class="p-front-about__img" src="../img/blog2.jpg">
                     </div>    
                 </section>
                 <section class="p-front-blog">
                     <div class="c-title-blog">
-                        <!--<img class="c-title-blog__img1" src="../img/GerberaPNG/g02.png">
-                        <img class="c-title-blog__img2" src="../img/GerberaPNG/g03.png">-->
+                        <img class="c-title-blog__img1" src="../img/GerberaPNG/g02.png">
+                        <img class="c-title-blog__img2" src="../img/GerberaPNG/g03.png">
                         <h2><span>Blog</span></h2>
                     </div>
                     <div class="p-front-blog1">
                         <div class="p-front-blog1__title">
-                            <img src="<?php echo esc_url( get_template_directory_uri() );?>/img/blog1.jpg">
+                            <img src="../img/blog1.jpg">
                             <p class="blog-title"><a href="#">テキストテキストテキスト</a></p>
                         </div>
                         <div class="p-front-blog1__title">
-                            <img src="<?php echo esc_url( get_template_directory_uri() );?>/img/blog1.jpg">
+                            <img src="../img/blog1.jpg">
                             <p class="blog-title"><a href="#">テキストテキストテキスト</a></p>
                         </div>
                         <div class="p-front-blog1__title">
-                            <img src="<?php echo esc_url( get_template_directory_uri() );?>/img/blog1.jpg" class="blog-img1">
+                            <img src="../img/blog1.jpg" class="blog-img1">
                             <p class="blog-title blog-title-sp"><a href="#">テキストテキストテキスト</a></p>
                         </div>
                     </div>
                 </section>
+            </main>-->
+            <!--<div class="l-Header__title">
+                    <img src="../img/One-Off logo black[708].png">
+                </div>
+                <div class="l-Header__link">
+                    <div class="c-Nav">
+                        <nav class="c-Nav__body">
+                            <ul id="menu-global-navi" class="c-Nav__body__menu">
+                                <li>
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+                                <li>
+                                    <a href="archive.html">Blog</a>
+                                </li>
+                                <li>
+                                    <a href="shop.html">Shop</a>
+                                </li>
+                                <li>
+                                    <a href="inquiry.html">Contact</a>
+                                </li>
+                            </ul>
+                            <ul id="sp-menu-global-navi" class="c-Nav__body__spmenu">
+                                <li>
+                                    <a href="index.html"><i class="fa-solid fa-house"></i><br>
+                                    <span class="iconname">Home</span></a>
+                                </li>
+                                <li>
+                                    <a href="about.html"><i class="fa-solid fa-circle-info"></i><br>
+                                    <span class="iconname">About</span></a>
+                                </li>
+                                <li>
+                                    <a href="archive.html"><i class="fa-solid fa-pen-to-square"></i><br>
+                                    <span class="iconname">Blog</span></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-solid fa-cart-shopping"></i><br>
+                                    <span class="iconname">Shop</span></a>
+                                </li>
+                                <li>
+                                    <a href="inquiry.html"><i class="fa-solid fa-envelope"></i><br>
+                                    <span class="iconname">Contact</span></a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div class="c-Nav__icon">
+                            <i class="fa-brands fa-facebook-square"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
+                    </div>
+                </div>
+            </header>-->
+            <main class="l-Main">
+                <div class="l-Main-wrapper">
+                  <article class="p-blog">
+                    <header class="p-blog-info c-title">
+                        <div class="c-title-blogtitle">
+                            <!--<img class="c-title-blogtitle__img1" src="../img/GerberaPNG/g02.png">
+                            <img class="c-title-blogtitle__img2" src="../img/GerberaPNG/g03.png">-->
+                            <h2><span>お店のお知らせです。</span></h2>
+                        </div>
+                        <p class="p-blog-date">4/7<span>2022</span></p>
+                        <p class="p-blog-cat">カテゴリー:お店の紹介</p>
+                    </header>
+                    <div class="p-blog-mv">
+                        <img src="../img/sakura-series.jpg" alt="新作商品">
+                        <p>テキストテキストテキストテキストテキストテキストテキストテキスト
+                        テキストテキストテキストテキストテキストテキストテキストテキスト
+                        </p>
+                    </div>
+                  </article>
+                  <aside class="l-sidebar p-blog-side">
+                    <div class="l-sidebar-menu">
+                        <div class="p-blog-side__title">
+                            <!--<img src="../img/GerberaPNG/g02.png">
+                            <img src="../img/GerberaPNG/g03.png">-->
+                            <h3><span>カテゴリー</span></h3>
+                        </div>
+                        <ul class="p-blog-side__archivemenu">
+                            <li><a href="#">新着情報</a></li>
+                            <li><a href="#">お知らせ</a></li>
+                            <li><a href="#">イヤリング</a></li>
+                            <li><a href="#">ネックレス</a></li>
+                        </ul>
+                    </div>
+                    <div class="l-sidebar-list">
+                        <div class="p-blog-side__archivetitle">
+                            <!--<img src="../img/GerberaPNG/g02.png">
+                            <img src="../img/GerberaPNG/g03.png">-->
+                            <h3><span>月別アーカイブ</span></h3>
+                        </div>
+                        <ul class="p-blog-side__list">
+                            <li><a href="#">2022年4月 <span class="count">(1)</span></a></li>
+                            <li><a href="#">2022年3月 <span class="count">(14)</span></a></li>
+                            <li><a href="#">2022年2月 <span class="count">(1)</span></a></li>
+                        </ul>
+                    </div>
+                  </aside>
+                </div>
             </main>
         <?php get_footer();?>    
---
+++ single.php
@@ -0,0 +1,180 @@
+<?php get_header();?>   
+            <!--<main class="l-Main p-front c-title">
+                <section class="p-front-info">
+                
+                <article>
+                    <div class="c-title-info">
+                        <img class="c-title-info__img1" src="../img/GerberaPNG/g02.png">
+                        <img class="c-title-info__img2" src="../img/GerberaPNG/g03.png">
+                        <h2><span>新着情報</span></h2>
+                    </div>
+                    <div class="p-front-info1">
+                        <div class="p-front-info1__title">
+                            <img src="../img/sakura-series.jpg">
+                            <p class="info-title"><a href="#">テキストテキストテキスト</a></p>
+                        </div>
+                        <div class="p-front-info1__title">
+                            <img src="/img/sakura-series.jpg">
+                            <p class="info-title"><a href="#">テキストテキストテキスト</a></p>
+                        </div>
+                        <div class="p-front-info1__title">
+                            <img src="/img/sakura-series.jpg" class="info-img1">
+                            <p class="info-title info-title-sp"><a href="#">テキストテキストテキスト</a></p>
+                        </div>
+                    </div>
+                    </article>
+                        <p>記事はありません。</p>
+                </section>
+                <section class="p-front-about">
+                    <div class="c-title-about">
+                        <img class="c-title-about__img1" src="../img/GerberaPNG/g02.png">
+                        <img class="c-title-about__img2" src="../img/GerberaPNG/g03.png">
+                        <h2><span>About</span></h2>
+                    </div>
+                    <div class="p-front-about-info">
+                        <p>One-Off KAO(ワンオフ・カオ)は、<br>革のお花のアクセサリーショップです。<br>全てハンドメイドで、<br>大きな一枚革からはさみで<br>ひとつずつ切り染めてお花に仕立てています。<br>【美しい色合いと立体感】のある形が特徴です。</p>
+                        <img class="p-front-about__img" src="../img/blog2.jpg">
+                    </div>    
+                </section>
+                <section class="p-front-blog">
+                    <div class="c-title-blog">
+                        <img class="c-title-blog__img1" src="../img/GerberaPNG/g02.png">
+                        <img class="c-title-blog__img2" src="../img/GerberaPNG/g03.png">
+                        <h2><span>Blog</span></h2>
+                    </div>
+                    <div class="p-front-blog1">
+                        <div class="p-front-blog1__title">
+                            <img src="../img/blog1.jpg">
+                            <p class="blog-title"><a href="#">テキストテキストテキスト</a></p>
+                        </div>
+                        <div class="p-front-blog1__title">
+                            <img src="../img/blog1.jpg">
+                            <p class="blog-title"><a href="#">テキストテキストテキスト</a></p>
+                        </div>
+                        <div class="p-front-blog1__title">
+                            <img src="../img/blog1.jpg" class="blog-img1">
+                            <p class="blog-title blog-title-sp"><a href="#">テキストテキストテキスト</a></p>
+                        </div>
+                    </div>
+                </section>
+            </main>-->
+            <!--<div class="l-Header__title">
+                    <img src="../img/One-Off logo black[708].png">
+                </div>
+                <div class="l-Header__link">
+                    <div class="c-Nav">
+                        <nav class="c-Nav__body">
+                            <ul id="menu-global-navi" class="c-Nav__body__menu">
+                                <li>
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li>
+                                    <a href="about.html">About</a>
+                                </li>
+                                <li>
+                                    <a href="archive.html">Blog</a>
+                                </li>
+                                <li>
+                                    <a href="shop.html">Shop</a>
+                                </li>
+                                <li>
+                                    <a href="inquiry.html">Contact</a>
+                                </li>
+                            </ul>
+                            <ul id="sp-menu-global-navi" class="c-Nav__body__spmenu">
+                                <li>
+                                    <a href="index.html"><i class="fa-solid fa-house"></i><br>
+                                    <span class="iconname">Home</span></a>
+                                </li>
+                                <li>
+                                    <a href="about.html"><i class="fa-solid fa-circle-info"></i><br>
+                                    <span class="iconname">About</span></a>
+                                </li>
+                                <li>
+                                    <a href="archive.html"><i class="fa-solid fa-pen-to-square"></i><br>
+                                    <span class="iconname">Blog</span></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-solid fa-cart-shopping"></i><br>
+                                    <span class="iconname">Shop</span></a>
+                                </li>
+                                <li>
+                                    <a href="inquiry.html"><i class="fa-solid fa-envelope"></i><br>
+                                    <span class="iconname">Contact</span></a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div class="c-Nav__icon">
+                            <i class="fa-brands fa-facebook-square"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                        </div>
+                    </div>
+                </div>
+            </header>-->
+            <main class="l-Main">
+                <div class="c-title">
+                    <div class="c-title-blog">
+                    <!--<img class="c-title-blog__img1" src="../img/GerberaPNG/g02.png">
+                    <img class="c-title-blog__img2" src="../img/GerberaPNG/g03.png">-->
+                    <h2><span>Blog</span></h2>
+                    </div>
+                </div>
+                <section class="p-archive">
+                    <div class="p-archive-main">
+                        <div class="p-archive-main-img">
+                            <img src="../img/tulip.jpg">
+                        </div>
+                        <div class="p-archive-main-text">
+                            <a href="blog.html">
+                                <h3>タイトル</h3>
+                                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="p-archive-main">
+                        <div class="p-archive-main-img">
+                            <img src="../img/tulip.jpg">
+                        </div>
+                        <div class="p-archive-main-text">
+                            <a href="blog.html">
+                                <h3>タイトル</h3>
+                                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="p-archive-main">
+                        <div class="p-archive-main-img">
+                            <img src="../img/tulip.jpg">
+                        </div>
+                        <div class="p-archive-main-text">
+                            <a href="blog.html">
+                                <h3>タイトル</h3>
+                                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="p-archive-main">
+                        <div class="p-archive-main-img">
+                            <img src="../img/tulip.jpg">
+                        </div>
+                        <div class="p-archive-main-text">
+                            <a href="blog.html">
+                                <h3>タイトル</h3>
+                                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+                            </a>
+                        </div>
+                    </div>
+                </section>
+                <div class="p-archive-page">
+                    <ul class="p-archive-page-pagenation">
+                        <li><a href="#">＜</a></li>
+                        <li><a href="#">Prev</a></li>
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">Next</a></li>
+                        <li><a href="#">＞</a></li>
+                    </ul>
+                </div>
+            </main>
+        <?php get_footer();?>    
