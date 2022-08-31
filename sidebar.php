e6652d221193b9da326c4f8cb6363e2901b4bd11
makoy <yoshidamaryoshidmarad1234@gmail.com>
2022-08-31 23:48:41 +0900

    sidebar.phpの作成とブログの投稿ページへの組み込み（完了）

---
+++ sidebar.php
@@ -0,0 +1,39 @@
+<aside class="l-sidebar p-blog-side">
+    <div class="l-sidebar-menu">
+                    <?php
+                        if ( is_active_sidebar( 'category_widget' ) ) :
+                            dynamic_sidebar( 'category_widget' );
+                        else:
+                    ?>
+                        <!--<div class="p-blog-side__title">
+                            <img src="../img/GerberaPNG/g02.png">
+                            <img src="../img/GerberaPNG/g03.png">
+                            <h3><span>カテゴリー</span></h3>
+                        </div>
+                        <ul class="p-blog-side__archivemenu">
+                            <li><a href="#">新着情報</a></li>
+                            <li><a href="#">お知らせ</a></li>
+                            <li><a href="#">イヤリング</a></li>
+                            <li><a href="#">ネックレス</a></li>
+                        </ul>-->
+                        <?php endif;?>
+    </div>
+    <div class="l-sidebar-list">
+                    <?php
+                        if ( is_active_sidebar( 'archive_widget' ) ) :
+                            dynamic_sidebar( 'archive_widget' );
+                        else:
+                    ?>
+                        <!--<div class="p-blog-side__archivetitle">
+                            <img src="../img/GerberaPNG/g02.png">
+                            <img src="../img/GerberaPNG/g03.png">
+                            <h3><span>月別アーカイブ</span></h3>
+                        </div>
+                        <ul class="p-blog-side__list">
+                            <li><a href="#">2022年4月 <span class="count">(1)</span></a></li>
+                            <li><a href="#">2022年3月 <span class="count">(14)</span></a></li>
+                            <li><a href="#">2022年2月 <span class="count">(1)</span></a></li>
+                        </ul>-->
+                        <?php endif;?>
+    </div>
+</aside>
--- single.php
+++ single.php
@@ -113,7 +113,7 @@
             </header>-->
             <main class="l-Main">
                 <div class="l-Main-wrapper">
-                  <article class="p-blog">
+                <article class="p-blog">
                     <?php while (have_posts()):the_post();?>
                     <header class="p-blog-info c-title">
                         <div class="c-title-blogtitle">
@@ -134,46 +134,8 @@
                         </p>-->
                     </div>
                     <?php endwhile;?>
-                  </article>
-                  <aside class="l-sidebar p-blog-side">
-                    <?php
-                        if ( is_active_sidebar( 'category_widget' ) ) :
-                            dynamic_sidebar( 'category_widget' );
-                        else:
-                    ?>
-                    <div class="l-sidebar-menu">
-                        <!--<div class="p-blog-side__title">
-                            <img src="../img/GerberaPNG/g02.png">
-                            <img src="../img/GerberaPNG/g03.png">
-                            <h3><span>カテゴリー</span></h3>
-                        </div>
-                        <ul class="p-blog-side__archivemenu">
-                            <li><a href="#">新着情報</a></li>
-                            <li><a href="#">お知らせ</a></li>
-                            <li><a href="#">イヤリング</a></li>
-                            <li><a href="#">ネックレス</a></li>
-                        </ul>-->
-                    </div>
-                    <?php endif;?>
-                    <?php
-                        if ( is_active_sidebar( 'archive_widget' ) ) :
-                            dynamic_sidebar( 'archive_widget' );
-                        else:
-                    ?>
-                    <div class="l-sidebar-list">
-                        <!--<div class="p-blog-side__archivetitle">
-                            <img src="../img/GerberaPNG/g02.png">
-                            <img src="../img/GerberaPNG/g03.png">
-                            <h3><span>月別アーカイブ</span></h3>
-                        </div>
-                        <ul class="p-blog-side__list">
-                            <li><a href="#">2022年4月 <span class="count">(1)</span></a></li>
-                            <li><a href="#">2022年3月 <span class="count">(14)</span></a></li>
-                            <li><a href="#">2022年2月 <span class="count">(1)</span></a></li>
-                        </ul>-->
-                    </div>
-                    <?php endif;?>
-                  </aside>
+                </article>
+                <?php get_sidebar();?>  
                 </div>
             </main>
         <?php get_footer();?>    
